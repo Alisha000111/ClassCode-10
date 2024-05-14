fbbb79de634b650e43aad277203a7730bc201626
A-M-I <160208713+Alisha000111@users.noreply.github.com>
2024-05-14 12:27:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+//console.log("hello objects!");
+//Objects
+//const myProperties = {
+//address{
+//city: "karachi",
+//country: "pakistan",
+//postalcode: 010101,
+//},
+//name: "Alisha",
+//rollNo: 101010,
+//age: 20,
+//}
+//console.log(myProperties.addresss.country);
+//const userPrompt = "name"
+//console.log(myProperties[userPrompt]);
+//const myProperties = ["alisha","pata nhi","101010","20"]
+//console.log(myProperties[1]);
+//let rollNo:string | number |boolean;
+//rollNo = "PIAIC-198060";
+//rollNo = 123456789;
+//rollNo = false;
+//const myProperties = {
+//name:string
+//age:number,
+//address:string,
+//rollNo:number,
+//}={
+//name: "Alisha",
+//rollNo: 101010,
+//age: 20,
+//address{
+//city: "karachi",
+//country: "pakistan",
+//postalcode: 010101,
+//},
+//console.log(myProperties);
